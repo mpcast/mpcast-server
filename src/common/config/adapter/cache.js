3b9b44d0322856faf3caa57bca8ec9caf804d354
baisheng@gmail.com <baisheng@gmail.com>
2018-04-07 21:57:35 +0800

    Update redis

--- src/common/config/adapter/cache.js
+++ src/common/config/adapter/cache.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   redis: {
     handle: redisCache,
-    port: isDev ? 6377 : 6380,
+    port: isDev ? 6379 : 6377,
     host: isDev ? '127.0.0.1' : '114.55.230.6',
     password: isDev ? '' : '__@caixie-redis_v2'
   }
