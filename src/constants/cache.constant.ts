01c8c378224e25dfadb88a89d680e9cbb1f2c22a
Baisheng <baisheng@gmail.com>
2019-04-22 16:53:57 +0800

    fix

--- src/constants/cache.constant.ts
+++ src/constants/cache.constant.ts
@@ -10,6 +10,4 @@ export const WECHAT = CACHE_PERFIX + 'WECHAT:';
 export const WECHAT_SESSION_KEY = WECHAT + 'SESSION_KEY:';
 export const INFO = CACHE_PERFIX + 'info';
 // export const TAGS = CACHE_PERFIX + 'tags';
-// export const HOT_ARTICLES = CACHE_PERFIX + 'hot-articles';
-// export const SITEMAP = CACHE_PERFIX + 'sitemap';
 // export const CONSTANTS = CACHE_PERFIX + 'constants';
